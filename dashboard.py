e7b36b2aabcd891a87ae120f6ecdbed57c4c9c13
akufanky <fankyab@gmail.com>
2024-08-02 14:43:10 +0700

    revisi

--- dashboard.py
+++ dashboard.py
@@ -97,6 +97,31 @@ rfm_df = create_rfm_df(main_df)
 
 st.header('Dicoding Collection Dashboard :sparkles:')
 
+st.subheader('Daily Orders')
+ 
+col1, col2 = st.columns(2)
+ 
+with col1:
+    total_orders = daily_orders_df.order_count.sum()
+    st.metric("Total orders", value=total_orders)
+ 
+with col2:
+    total_revenue = format_currency(daily_orders_df.revenue.sum(), "AUD", locale='es_CO') 
+    st.metric("Total Revenue", value=total_revenue)
+ 
+fig, ax = plt.subplots(figsize=(16, 8))
+ax.plot(
+    daily_orders_df["order_date"],
+    daily_orders_df["order_count"],
+    marker='o', 
+    linewidth=2,
+    color="#90CAF9"
+)
+ax.tick_params(axis='y', labelsize=20)
+ax.tick_params(axis='x', labelsize=15)
+ 
+st.pyplot(fig)
+
 st.subheader('Best & Worst Performing Product')
 
 fig, ax = plt.subplots(nrows=1, ncols=2, figsize=(35,15))
@@ -106,7 +131,7 @@ colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 sns.barplot(x='quantity_x', y='product_name', data=sum_order_items_df.head(5), palette=colors, ax=ax[0])
 ax[0].set_ylabel(None)
 ax[0].set_xlabel('Number of Sales', fontsize=30)
-ax[0].set_title('Best Performing Product', loc='center', fintsize=50)
+ax[0].set_title('Best Performing Product', loc='center', fontsize=50)
 ax[0].tick_params(axis='y', labelsize=35)
 ax[0].tick_params(axis='x', labelsize=30)
 
@@ -116,7 +141,7 @@ ax[1].set_xlabel('Number of Sales', fontsize=30)
 ax[1].invert_xaxis()
 ax[1].yaxis.set_label_position('right')
 ax[1].yaxis.tick_right()
-ax[1].set_title('Worst Performing Product', loc='center', fintsize=50)
+ax[1].set_title('Worst Performing Product', loc='center', fontsize=50)
 ax[1].tick_params(axis='y', labelsize=35)
 ax[1].tick_params(axis='x', labelsize=30)
 
@@ -128,6 +153,8 @@ col1, col2 = st.columns(2)
 
 with col1:
     fig, ax = plt.subplots(figsize=(20, 10))
+
+    colors = ["#90CAF9", "#D3D3D3", "#D3D3D3", "#D3D3D3", "#D3D3D3"]
 
     sns.barplot(
         y='customer_count',
@@ -141,6 +168,7 @@ with col1:
     ax.set_xlabel(None)
     ax.tick_params(axis='x', labelsize=35)
     ax.tick_params(axis='y', labelsize=30)
+    st.pyplot(fig)
 
 with col2:
     fig, ax = plt.subplots(figsize=(20, 10))
